7a77b4fcb064e7bb8f0db87460386972bf67ca2d
Antonio Dinkins <afdinki@gmail.com>
2021-08-03 12:50:27 -0400

    Modified Product Routes

--- server/api/products.js
+++ server/api/products.js
@@ -11,12 +11,17 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get(':/id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
       let productId = req.params.id;
       let product = await Product.findByPk(productId);
-      res.status(200).json(product);
-  } catch(error) {
+      if(product){
+        res.status(200).json(product)
+      } else {
+        res.status(404).send("Product Not Found")
+       // throw new Error("Product Not Found");
+      }
+     } catch (error){
     next(error)
   }
 })
@@ -35,19 +40,32 @@ router.put('/:id', async (req, res, next) => {
       let newProduct = req.body;
       let {id} = req.params;
       let  product = await Product.findByPk(id);
-      let updateProduct = await product.update(newProduct);
-      res.status(200).json(updatedProduct);
+      if(product){
+        let updatedProduct = await product.update(newProduct);
+        res.status(200).json(updatedProduct);
+      } else {
+
+        res.status(404).send("Product Not Found")
+        // throw new Error("Product Not Found")
+      }
+
+
   } catch(error) {
     next(error)
   }
 })
 
-router.delete('/', async (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   try {
       const product = await Product.findByPk(req.params.id);
-      await product.destory();
-      res.send(product);
-  } catch(error) {
+      if(product){
+        await product.destroy();
+       res.status(202).send(product);
+      } else {
+        res.status(404).send("Product not found")
+      }
+
+  } catch(error){
     next(error);
   }
 })
