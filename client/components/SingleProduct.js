58e6a55d9263359d137e93b94582c9247a71efb4
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-03 10:09:16 -0700

    Create single-product component

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,50 @@
+// Import modules
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Import Redux action & thunk creators
+import { _getProduct, getProduct } from '../store/singleProduct';
+
+// Define component
+class SingleProduct extends React.Component {
+  componentDidMount() {
+    this.props.getProduct(this.props.match.params.robotId);
+  }
+
+  componentWillUnmount() {
+    this.props.resetProduct();
+  }
+
+  render() {
+    const { product } = this.props;
+
+    if (!product.id) return <div>Loading...</div>;
+
+    return (
+      <div>
+        <img src={product.imageUrl} />
+        <p>{product.name}</p>
+        <p>${product.price}</p>
+        <p>Description: {product.description}</p>
+      </div>
+    );
+  }
+}
+
+// Connect Redux store's state to props
+const mapState = (state) => {
+  return {
+    product: state.singleProduct,
+  };
+};
+
+// Connect Redux store's action/thunk creators to props
+const mapDispatch = (dispatch) => {
+  return {
+    getProduct: (productId) => dispatch(getProduct(productId)),
+    resetProduct: () => dispatch(_getProduct({})),
+  };
+};
+
+// Export redux-connected component
+export default connect(mapState, mapDispatch)(SingleProduct);
