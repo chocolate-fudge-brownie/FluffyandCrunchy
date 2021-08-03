97febae987ab94d9947fd7656d1a9700e852000d
Galower <ariosgalower@gmail.com>
2021-08-03 16:41:50 -0400

    removing unneeded import

--- client/store/cart.js
+++ client/store/cart.js
@@ -2,8 +2,6 @@
  * ACTION TYPES
  */
 
-import { getProducts } from './products';
-
 const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
 const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODUCT_FROM_CART';
 const GET_CART_PRODUCTS = 'GET_CARD_PRODUCTS';
