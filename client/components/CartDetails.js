e1b5946593284f3d01b2a0d84c99e3874b93ff2b
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-03 14:04:30 -0700

    Create cart-details component

---
+++ client/components/CartDetails.js
@@ -0,0 +1,51 @@
+// Import modules
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Import Redux action & thunk creators
+import { getCartProducts } from '../store/cart';
+
+// Define component
+class CartPreview extends React.Component {
+  componentDidMount() {
+    this.props.getCartProducts();
+  }
+
+  render() {
+    const { cart } = this.props;
+
+    return (
+      <div>
+        {cart.map((product) => (
+          <div key={product.id}>
+            <Link to={`/product/${product.id}`}>
+              <img src={product.imageUrl} />
+            </Link>
+            <Link to={`/products/${product.id}`}>
+              <p>{product.name}</p>
+            </Link>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+// Connect Redux store's state to props
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.auth.id,
+    cart: state.cart,
+  };
+};
+
+// Connect Redux store's action/thunk creators to props
+const mapDispatch = (dispatch) => {
+  return {
+    getCartProducts: () => dispatch(getCartProducts()),
+  };
+};
+
+// Export redux-connected component
+export default connect(mapState, mapDispatch)(CartPreview);
