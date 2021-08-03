8d66bb95173910e205e590b6218c2e7572f6eced
Galower <ariosgalower@gmail.com>
2021-08-03 17:00:00 -0400

    clear storage functionality for when the user logs out

--- client/store/cart.js
+++ client/store/cart.js
@@ -2,13 +2,16 @@
  * ACTION TYPES
  */
 
-const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
 const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODUCT_FROM_CART';
 const GET_CART_PRODUCTS = 'GET_CARD_PRODUCTS';
+const CLEAR_STORAGE = 'CLEAR_STORAGE';
 
 /**
  * ACTION CREATORS
  */
+export const _clearStorage = () => ({
+    type: CLEAR_STORAGE,
+});
 
 export const _addProductToCart = (product) => ({
     type: ADD_PRODUCT_TO_CART,
@@ -28,6 +31,11 @@ export const _getCartProducts = (products) => ({
 /**
  * THUNK CREATORS
  */
+
+export const clearStorage = () => (dispatch) => {
+    window.localStorage.clear();
+    dispatch(_clearStorage());
+};
 
 export const addProductToCart = (product) => (dispatch) => {
     if (!window.localStorage.getItem('products')) {
@@ -75,6 +83,8 @@ export default function (state = [], action) {
             return [...state, action.product];
         case REMOVE_PRODUCT_FROM_CART:
             return action.products;
+        case CLEAR_STORAGE:
+            return [];
         default:
             return state;
     }
