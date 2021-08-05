82e40caacc6c930f00bc31ab6aa932bd16830162
super-rogatory <chukwudi10@gmail.com>
2021-08-05 14:10:09 -0700

    getUser(), peekCart()

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -47,6 +47,17 @@ User.prototype.generateToken = function() {
   return jwt.sign({id: this.id}, process.env.JWT)
 }
 
+User.prototype.getCart = async function () {
+  const orders = await this.getOrders();
+  const customerId = orders[0].customerId;
+  const cart = await Order.findOne({
+    where: {
+      id: customerId,
+      isPaid: false
+    }
+  });
+  return cart;
+}
 /**
  * classMethods
  */
@@ -76,7 +87,7 @@ User.findByToken = async function(token) {
   }
 }
 User.peekCart = async function(user) {
-  // only works because username is unique, should only be one Cart (Order with isPaid false)
+  // next step: addProductToOrder
   try {
     if(!user) throw new Error('failed to pass in a user as an argument');
     const { username } = user;
@@ -89,6 +100,7 @@ User.peekCart = async function(user) {
         }
     }});
     const cart = data.orders[0];
+    cart.dataValues['products'] = await cart.getProducts();
     return cart.dataValues;    
   } catch (err) {
     console.log(err);
@@ -112,3 +124,52 @@ User.afterCreate(async (user) => {
 })
 
 /* after create hook for associating a new user with an unpaid order */
+
+
+/* peekCart documentation - class method
+  - Parameters: a user instance
+  - Return Value: the order object whose isPaid value is set to False. We will refer to this as the cart. Recall, there is only one cart per user, but a user can have many orders.
+  - NOTICE: Inside the cart object is a products array that contains all of the products associated with the cart.
+  - Previously, I used my own priceUpdate() method on the Order Model to add a product to the cart.
+  - example: 
+      const user = await User.create({ username: 'Chukwudi', password: 'password', admin: false });
+
+          const fluffs = await Product.create({ name: 'fluffs', price: 450 });
+          const crunchies = await Product.create({ name: 'crunchies', price: 200 });
+
+          await User.peekCart(user);
+          const orders = await user.getOrders(); // this is how we access the cart.
+          const cart = orders[0]; // recall that the cart is supposed to exist on index 0 of the return value in getOrders(). this is the cart that is automatically created after the user is created
+          await cart.priceUpdate(fluffs);
+          const newCart = await User.peekCart(user);
+[#1]      console.log(newCart);
+  - You can also destructure the products out of the return object from peekCart()
+  - example: { products } = await User.peekCart(user)
+
+  - Below you will find an example output from [#1]
+  {
+  id: 1,
+  total: 450,
+  isPaid: false,
+  createdAt: 2021-08-05T20:24:52.767Z,
+  updatedAt: 2021-08-05T20:24:52.791Z,
+  customerId: 1,
+  products: [
+    product {
+      dataValues: [Object],
+      _previousDataValues: [Object],
+      _changed: Set {},
+      _options: [Object],
+      isNewRecord: false,
+      OrderLine: [OrderLine]
+    }
+  ]
+}
+
+*/
+
+/* getCart documentation - instance method
+  - Parameter: no parameters
+  - Return Value: returns the cart object (an order object with isPaid equal to false)
+
+*/
