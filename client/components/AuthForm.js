803ff9c6b2f623d3990b44d844844fbf3c87c939
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-05 11:57:16 -0700

    React - add email input field in sign-up view
    users are now required to sign up by username & email

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,45 +1,58 @@
-import React from "react";
-import { connect } from "react-redux";
-import { authenticate } from "../store";
+import React from 'react';
+import { connect } from 'react-redux';
+import { authenticate } from '../store';
 
 /**
  * COMPONENT
  */
 const AuthForm = (props) => {
-    const { name, displayName, handleSubmit, error } = props;
+  const { name, displayName, handleSubmit, error } = props;
 
-    return (
-        <div className="main-form">
-            <form onSubmit={handleSubmit} name={name}>
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label">
-                        Username
-                    </label>
-                    <input
-                        name="username"
-                        type="text"
-                        id="username"
-                        className="form-control"
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">
-                        Password
-                    </label>
-                    <input
-                        name="password"
-                        type="password"
-                        id="password"
-                        className="form-control"
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    {displayName}
-                </button>
-                {error && error.response && <div> {error.response.data} </div>}
-            </form>
+  return (
+    <div className="main-form">
+      <form onSubmit={handleSubmit} name={name}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            name="username"
+            type="text"
+            id="username"
+            className="form-control"
+          />
         </div>
-    );
+        {name === 'signup' ? (
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              name="email"
+              type="text"
+              id="email"
+              className="form-control"
+            />
+          </div>
+        ) : null}
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            id="password"
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          {displayName}
+        </button>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
+  );
 };
 
 /**
@@ -50,31 +63,32 @@ const AuthForm = (props) => {
  *   can stay DRY with interfaces that are very similar to each other!
  */
 const mapLogin = (state) => {
-    return {
-        name: "login",
-        displayName: "Login",
-        error: state.auth.error,
-    };
+  return {
+    name: 'login',
+    displayName: 'Login',
+    error: state.auth.error,
+  };
 };
 
 const mapSignup = (state) => {
-    return {
-        name: "signup",
-        displayName: "Sign Up",
-        error: state.auth.error,
-    };
+  return {
+    name: 'signup',
+    displayName: 'Sign Up',
+    error: state.auth.error,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-    return {
-        handleSubmit(evt) {
-            evt.preventDefault();
-            const formName = evt.target.name;
-            const username = evt.target.username.value;
-            const password = evt.target.password.value;
-            dispatch(authenticate(username, password, formName));
-        },
-    };
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const username = evt.target.username.value;
+      const email = evt.target.email ? evt.target.email.value : '';
+      const password = evt.target.password.value;
+      dispatch(authenticate(username, email, password, formName));
+    },
+  };
 };
 
 export const Login = connect(mapLogin, mapDispatch)(AuthForm);
