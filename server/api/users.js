b558e887b324267a7654719c2c1ec25cd638e034
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-06 12:39:37 -0700

    fix bugs of missing modules

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,9 @@
-const { models: { User, Order }} = require('../db')
-const {isAdmin, requireToken} = require('./gatekeepingMiddleware')
-
+const express = require('express');
+const router = express.Router();
+const {
+  models: { User, Order },
+} = require('../db');
+const { isAdmin, requireToken } = require('./gatekeepingMiddleware');
 
 router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
@@ -8,26 +11,25 @@ router.get('/', requireToken, isAdmin, async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ['id', 'username'],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.get('/:id', requireToken, async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.id, {include: Order});
-    if(user){
-      res.json(user)
+    const user = await User.findByPk(req.params.id, { include: Order });
+    if (user) {
+      res.json(user);
     } else {
-      res.status(404).send("User not found")
+      res.status(404).send('User not found');
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
-
+module.exports = router;
