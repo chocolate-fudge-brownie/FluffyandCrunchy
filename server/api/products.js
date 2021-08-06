cd4d8f8c0ac1b0c18487fe999d4e2053d490c2db
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-06 14:10:00 -0700

    Check all products API routes
    now only admins could add, update and remove products

--- server/api/products.js
+++ server/api/products.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const router = express.Router();
-const {models: { Product } }= require('../db')
-const {requireToken, isAdmin} = require( './gatekeepingMiddleware')
+// Import modules
+const express = require('express');
 
+// Import gatekeeping middlewares
+const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
+
+// Import models
+const {
+  models: { Product },
+} = require('../db');
+
+// Create sub-router mounted on /api/products
+const router = express.Router();
+
+// GET /api/products
+// GET all products
 router.get('/', async (req, res, next) => {
   try {
     let products = await Product.findAll();
@@ -10,68 +21,67 @@ router.get('/', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-})
+});
 
-router.get('/:id', async (req, res, next) => {
+// GET /api/products/:productId
+// Get single product
+router.get('/:productId', async (req, res, next) => {
   try {
-      let productId = req.params.id;
-      let product = await Product.findByPk(productId);
-      if(product){
-        res.json(product)
-      } else {
-        res.status(404).send("Product Not Found")
-       // throw new Error("Product Not Found");
-      }
-     } catch (error){
-    next(error)
+    let { productId } = req.params;
+    let product = await Product.findByPk(productId);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).send('Product Not Found');
+    }
+  } catch (error) {
+    next(error);
   }
-})
+});
 
+// POST /api/products
+// Add a product (admin only)
 router.post('/', requireToken, isAdmin, async (req, res, next) => {
   try {
     const product = await Product.create(req.body);
     res.status(201).json(product);
-  } catch(error) {
-    next(error)
-  }
-})
-
-router.put('/:id', requireToken, isAdmin, async (req, res, next) => {
-  try {
-      let newProduct = req.body;
-      let {id} = req.params;
-      let  product = await Product.findByPk(id);
-      if(product){
-        let updatedProduct = await product.update(newProduct);
-        res.json(updatedProduct);
-      } else {
-
-        res.status(404).send("Product Not Found")
-        // throw new Error("Product Not Found")
-      }
-
-
-  } catch(error) {
-    next(error)
-  }
-})
-
-router.delete('/:id', requireToken, isAdmin, async (req, res, next) => {
-  try {
-      const product = await Product.findByPk(req.params.id);
-      if(product){
-        await product.destroy();
-       res.status(202).send(product);
-      } else {
-        res.status(404).send("Product not found")
-      }
-
-  } catch(error){
+  } catch (error) {
     next(error);
   }
-})
+});
 
+// PUT /api/products/:productId
+// Update a product (admin only)
+router.put('/:productId', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    let newProduct = req.body;
+    let { productId } = req.params;
+    let product = await Product.findByPk(productId);
+    if (product) {
+      let updatedProduct = await product.update(newProduct);
+      res.json(updatedProduct);
+    } else {
+      res.status(404).send('Product Not Found');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-
+// DELETE /api/products/:productId
+// Delete a product (admin only)
+router.delete('/:id', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if (product) {
+      await product.destroy();
+      res.status(202).send(product);
+    } else {
+      res.status(404).send('Product Not Found');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
