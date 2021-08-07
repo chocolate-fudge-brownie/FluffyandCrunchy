9d23eb8dd0d23ad12eb632fa444a6521da37e42b
super-rogatory <chukwudi10@gmail.com>
2021-08-07 12:27:01 -0700

    Encapsulated implementation into a class

--- script/seed.js
+++ script/seed.js
@@ -249,68 +249,69 @@ async function seed() {
     })
   ]);
 
-  // helper functions
+  class PokemonSeed {
+    constructor(queryLimit) {
+      this.QUERY_LIMIT = queryLimit;
+    }
+    get queryLimit() {
+      return this.QUERY_LIMIT;
+    }
+    // returns 50 pokemon objects with name and url
+    async getNames() {
+      try {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${this.queryLimit}`);
+        return data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    // returns an description given an id
+    async getDescription(id) {
+      try {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
+        const englishEntry = data.flavor_text_entries
+                              .filter((description) => description.language.name === "en")[0].flavor_text;
+        return englishEntry;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getImageUrl(id) {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+    };
+    // returns a random price
+    randomPrice() {
+      return Math.floor(faker.commerce.price());
+    };
+    // factory function to generate pokemon objects
+    factoryPokemon(name, price, imageUrl, description) {
+      return {
+        name,
+        price,
+        imageUrl,
+        description,
+      };
+    };
+    async generatePokemonObjects() {
+      const pokemonNames = await this.getNames();
+      for(let i = 1; i <= this.queryLimit; i++) {
+        const name = pokemonNames[i - 1].name;
+        const imageUrl = this.getImageUrl(i); 
+        const description = await this.getDescription(i);
+        const pokemonProduct = this.factoryPokemon(name, this.randomPrice(), imageUrl, description);
+        products.push(await Promise.resolve(Product.create(pokemonProduct)));
+      }   
+    }
+  }
 
-  // SETS QUERY LIMIT - can change how many pokemon we want seeded into the database simply by increasing or decreasing this number.
+  // actually seeds and appends pokemon objects to the database, needs a QUERY LIMIT
   // NOTE: The more pokemon objects we create, the longer the seed will take. 50 -> ~5-7 seconds, 100 -> ~20-30 seconds
-  const QUERY_LIMIT = 50;
-
-  // returns 50 pokemon objects with name and url
-  const getNames = async () => {
-    try {
-      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${QUERY_LIMIT}`);
-      return data.results;      
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  // returns an description given an id
-  const getDescription = async (id) => {
-    try {
-      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
-      const englishEntry = data.flavor_text_entries.filter((description) => description.language.name === 'en')[0].flavor_text;
-      return englishEntry;      
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  // returns imageUrl
-  const getImageUrl = (id) => {
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-  }
-
-  // returns a random price
-  const randomPrice = () => {
-    return Math.floor(faker.commerce.price());
-  }
-
-  // factory function to generate pokemon objects
-  const factoryPokemon = (name, price, imageUrl, description) => {
-    return {
-      name,
-      price,
-      imageUrl,
-      description
-    }
-  }
-
-  // main pokemon object generator function
-  async function generatePokemonObjects() {
-    const pokemonNames = await getNames();
-    for(let i = 1; i <= QUERY_LIMIT; i++) {
-      const name = pokemonNames[i - 1].name;
-      const imageUrl = getImageUrl(i); 
-      const description = await getDescription(i);
-      const pokemonProduct = factoryPokemon(name, randomPrice(), imageUrl, description);
-      products.push(await Promise.resolve(Product.create(pokemonProduct)));
-    }   
-  }
+  const pokeSeed = new PokemonSeed(25);
+  await pokeSeed.generatePokemonObjects();
+   
+  /*  
   
-  // actually seeds and appends pokemon objects to the database
-  await generatePokemonObjects();
-
+  */
   console.log(`seeded ${users.length} users`);
   console.log(`seeded ${products.length} products`);
   /* Try to add order for users */
