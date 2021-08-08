828a789d944e93882ba2a67c7b337e6064c62701
Adrian <ariosgalower@gmail.com>
2021-08-08 17:52:05 -0400

    implemented search bar

--- client/store/products.js
+++ client/store/products.js
@@ -24,42 +24,42 @@ const _createProduct = (product) => ({ type: CREATE_PRODUCT, product });
  */
 
 export const createProduct = (product, history) => async (dispatch) => {
-    try {
-        const { data } = await axios.post('/api/products', product);
-        dispatch(_createProduct(data));
-        history.push('/products');
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const { data } = await axios.post('/api/products', product);
+    dispatch(_createProduct(data));
+    history.push('/products');
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getProducts = () => async (dispatch) => {
-    try {
-        const { data } = await axios.get('/api/products');
-        dispatch(_getProducts(data));
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const { data } = await axios.get('/api/products');
+    dispatch(_getProducts(data));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const deleteProduct = (id, history) => async (dispatch) => {
-    try {
-        const { data } = await axios.delete(`/api/products/${id}`);
-        dispatch(_deleteProduct(data));
-        history.push('/products');
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const { data } = await axios.delete(`/api/products/${id}`);
+    dispatch(_deleteProduct(data));
+    history.push('/products');
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const updateProduct = (id, product, history) => async (dispatch) => {
-    try {
-        const { data } = await axios.put(`/api/products/${id}`, product);
-        dispatch(_updateProduct(data));
-        history.push('/products');
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const { data } = await axios.put(`/api/products/${id}`, product);
+    dispatch(_updateProduct(data));
+    history.push('/products');
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 /**
@@ -67,18 +67,18 @@ export const updateProduct = (id, product, history) => async (dispatch) => {
  */
 
 export default function (state = [], action) {
-    switch (action.type) {
-        case CREATE_PRODUCT:
-            return [...state, action.product];
-        case GET_PRODUCTS:
-            return action.products;
-        case DELETE_PRODUCT:
-            return state.filter((product) => product.id !== action.product.id);
-        case UPDATE_PRODUCT:
-            return state.map((product) =>
-                product.id === action.product.id ? action.product : product
-            );
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CREATE_PRODUCT:
+      return [...state, action.product];
+    case GET_PRODUCTS:
+      return action.products;
+    case DELETE_PRODUCT:
+      return state.filter((product) => product.id !== action.product.id);
+    case UPDATE_PRODUCT:
+      return state.map((product) =>
+        product.id === action.product.id ? action.product : product
+      );
+    default:
+      return state;
+  }
 }
