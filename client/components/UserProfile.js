f5308e3fdbe67d064953bb93c483268a4507a0e5
hhlin83 <83198277+hhlin83@users.noreply.github.com>
2021-08-10 18:54:32 -0700

    Modify the buttons look in user-profile view

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -100,10 +100,10 @@ class UserProfile extends React.Component {
             </div>
           </div>
           {editMode ? (
-            <div>
+            <div id="cancel-submit-buttons">
               <button
                 type="button"
-                className="btn btn-primary"
+                className="btn btn-outline-primary"
                 onClick={cancelEditMode}
               >
                 Cancel
